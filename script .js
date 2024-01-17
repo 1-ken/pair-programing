173fd72d4aa746dcfd6ba639827005f277c8a5c3
keen <kennicmwimu@gmail.com>
2024-01-17 14:12:06 +0300

    done

---
+++ script .js
@@ -0,0 +1,12 @@
+function convertBinaryToDecimal() {
+  const binaryInput = document.getElementById("binaryinput").value;
+
+  // Validate binary input
+  if (/^[01]+$/.test(binaryInput)) {
+    const decimalEquivalent = parseInt(binaryInput, 2);
+    document.getElementById("result").innerText = `${decimalEquivalent}`;
+  } else {
+    document.getElementById("result").innerText =
+      "Invalid Binary Input. Enter 0s and 1s only.";
+  }
+}
